3e6fa61fa5676b7ff7d054337143e28bb90376f4
mihalw28 <michal.waszak@yahoo.com>
2019-10-03 16:34:23 +0200

    Update user interface, add appropriate routes, fix formatting.

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,12 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_wtf.csrf import CSRFProtect
 from config import DevelopmentConfig
-
-# app = Flask(__name__)
-# app.config.from_object(Config)
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
 
 db = SQLAlchemy()
 migrate = Migrate()
+csrf = CSRFProtect()
 
 
 def create_app(config_class=DevelopmentConfig):
@@ -18,6 +14,7 @@ def create_app(config_class=DevelopmentConfig):
     app.config.from_object(config_class)
     db.init_app(app)
     migrate.init_app(app, db)
+    csrf.init_app(app)
 
     from app.core import bp as core_bp
 
--- app/core/forms.py
+++ app/core/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField, TextAreaField
-from wtforms.validators import DataRequired, ValidationError, InputRequired
+from wtforms import IntegerField, StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, InputRequired, ValidationError
 
 
 class AddBookForm(FlaskForm):
@@ -9,6 +9,19 @@ class AddBookForm(FlaskForm):
     description = TextAreaField("Description", validators=[DataRequired()])
     pages_no = IntegerField("Number of pages", validators=[InputRequired()])
     submit = SubmitField("Add to library")
+
+    def validate_pages_no(self, pages_no):
+        """Method validates if number of pages is a positive one."""
+        if pages_no.data < 1:
+            raise ValidationError("A book cannot have less than 1 page.")
+
+
+class UpdateBookForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    author = StringField("Author", validators=[DataRequired()])
+    description = TextAreaField("Description", validators=[DataRequired()])
+    pages_no = IntegerField("Number of pages", validators=[InputRequired()])
+    submit = SubmitField("Update book")
 
     def validate_pages_no(self, pages_no):
         """Method validates if number of pages is a positive one."""
