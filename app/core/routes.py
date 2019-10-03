6920b79aa9b17e900c39f4c62f60c99f99d24b01
mihalw28 <michal.waszak@yahoo.com>
2019-10-04 00:47:11 +0200

    Add new routes with templates to share etries.

--- app/core/routes.py
+++ app/core/routes.py
@@ -1,3 +1,6 @@
+import datetime
+import uuid
+
 from flask import flash, redirect, render_template, request, session, url_for
 
 from app import db
@@ -71,9 +74,42 @@ def update_book():
 
 @bp.route("/view_book", methods=["GET", "POST"])
 def view_book():
-    """After click on button function renders particular book view."""
+    """On click button function renders particular book view."""
     if request.method == "POST":
         bk_id = request.form.get("id", type=int)
         book = Book.query.filter_by(id=bk_id).first()
         session["book_id"] = book.id
     return render_template("view_book.html", title="View", book=book)
+
+
+@bp.route("/book/<int:id>/<string:token>", methods=["GET"])
+def render_book_preview(id, token):
+    """Renders preview of book from link."""
+    book = Book.query.filter_by(id=id).first()
+    if (
+        book.link_generation_time + datetime.timedelta(days=0, seconds=300)
+        < datetime.datetime.utcnow()
+    ) or token != book.link_token:
+        return redirect(url_for("core.error"))
+    return render_template("link_book.html", title="Link book", book=book)
+
+
+@bp.route("/generate_link", methods=["GET", "POST"])
+def generate_link():
+    """On click button function generates link for single entry."""
+    if request.method == "POST":
+        bk_id = request.form.get("id", type=int)
+        book = Book.query.filter_by(id=bk_id).first()
+        book.link_generation_time = datetime.datetime.utcnow()
+        # Not necessary to create UUID object, string is enough
+        token = str(uuid.uuid4())[-12:]  # dozen-sign token
+        book.link_token = token
+        db.session.commit()
+        link = f"{request.host_url}book/{book.id}/{book.link_token}"
+    return render_template("view_book.html", title="View", book=book, link=link)
+
+
+@bp.route("/error")
+def error():
+    """Renders timeout_error template."""
+    return render_template("timeout_error.html", title="Error")
--- app/models.py
+++ app/models.py
@@ -6,11 +6,13 @@ from app import db
 class Book(db.Model):
     __tablename__ = "book"
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, unique=True, nullable=False)
-    author = db.Column(db.String, nullable=False)
-    description = db.Column(db.String, nullable=False)
-    pages_no = db.Column(db.Integer, nullable=False)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(1000), nullable=False)
+    pages_no = db.Column(db.String(5), nullable=False)
     added_date = db.Column(db.DateTime)
+    link_generation_time = db.Column(db.DateTime, nullable=True)
+    link_token = db.Column(db.String(20), unique=True, nullable=True)
 
     def __repr__(self):
         return f"<{self.__class__.__name__}({self.title})>"
